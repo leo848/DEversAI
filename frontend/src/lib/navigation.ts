2152248cb45bacac0568c61efd0b46d6ef32af0e
L. Blume <blume.l@posteo.de>
2024-12-13 06:33:35 +0100

    Update pnpm lock, add navigation page for upcoming navigation

---
+++ frontend/src/lib/navigation.ts
@@ -0,0 +1,12 @@
+export const pages = [
+	{
+		title: "Startseite",
+		link: "/",
+		icon: "flowbite:home-solid",
+	},
+	{
+		title: "Tokens",
+		link: "/token",
+		icon: "tabler:binary-tree-filled",
+	}
+]
