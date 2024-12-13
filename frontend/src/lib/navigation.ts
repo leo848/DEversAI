b1936e63bf481df0c0ce250236635ff2044e4b2a
L. Blume <blume.l@posteo.de>
2024-12-13 06:47:58 +0100

    Add basic navbar

--- frontend/src/lib/navigation.ts
+++ frontend/src/lib/navigation.ts
@@ -2,11 +2,16 @@ export const pages = [
 	{
 		title: "Startseite",
 		link: "/",
-		icon: "flowbite:home-solid",
+		icon: {
+			icon: "flowbite:home-solid",
+		}
 	},
 	{
 		title: "Tokens",
 		link: "/token",
-		icon: "tabler:binary-tree-filled",
+		icon: {
+			icon: "tabler:binary-tree-filled",
+			vFlip: true,
+		}
 	}
 ]
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,28 @@
 <script lang="ts">
 	import '../app.css';
+	import { pages } from '$lib/navigation';
+	import Icon from "@iconify/svelte";
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-row">
+	<nav class="flex flex-col fancy-gradient-bg gap-4 p-2">
+		{#each pages as page}
+			<div>
+				<button class="bg-white bg-opacity-50 hover:bg-opacity-100 rounded-xl p-2 transition">
+					<Icon {...page.icon} width="50px" />
+				</button>
+			</div>
+		{/each}
+	</nav>
+	<main class="grow">
+		{@render children()}
+	</main>
+</div>
+
+<style>
+	.fancy-gradient-bg {
+		background: linear-gradient(to bottom, #ffcc44 0%, #aa3300 100%)
+	}
+</style>
