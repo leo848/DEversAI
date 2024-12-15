55b03b2373775619c9fc70d4aa1352d0598f1c1c
L. Blume <blume.l@posteo.de>
2024-12-15 18:39:48 +0100

    Use correct source of randomness

--- frontend/src/routes/token/+page.svelte
+++ frontend/src/routes/token/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import demoVocabulary from '$lib/tokenizing/demoVocabulary';
+	import vocabulary from '$lib/tokenizing/german50000';
 
-	const randomToken = Math.floor(Math.random() * demoVocabulary.tokens.length);
+	const randomToken = Math.floor(Math.random() * vocabulary.tokens.length);
 
 	goto(`/token/${randomToken}`, { replaceState: true });
 </script>
