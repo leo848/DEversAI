9bd0aa7e27a643371db68b09d94a3e6e07060b31
L. Blume <blume.l@posteo.de>
2024-12-19 12:40:53 +0100

    Use border sections in tokenize/+page

--- frontend/src/routes/tokenize/+page.svelte
+++ frontend/src/routes/tokenize/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import BorderSection from '$lib/components/BorderSection.svelte';
 	import Token from '$lib/components/Token.svelte';
 	import vocabulary from '$lib/tokenizing/german50000';
 
@@ -12,23 +13,29 @@
 
 	<div class="grid grid-cols-12 gap-8">
 		<div class="col-span-8">
-			<textarea
-				class="w-full rounded-xl border-2 border-gray-200 p-4 text-2xl transition-all focus:border-gray-500"
-				bind:value={string}
-			>
-			</textarea>
+			<BorderSection title="Eingabe">
+				<textarea
+					class="w-full rounded-xl border-2 border-gray-200 p-4 text-2xl transition-all focus:border-gray-500"
+					bind:value={string}
+				>
+				</textarea>
+			</BorderSection>
 		</div>
 		<div class="col-span-4 row-span-2"></div>
 
-		<div class="col-span-8 flex flex-row flex-wrap gap-0 gap-y-4">
-			{#each tokens as token, index}
-				{@const color = token.isByte()
-					? 'gray'
-					: (['pastelBlue', 'pastelPink', 'pastelYellow', 'pastelGreen'] as const)[index % 4]}
-				<div>
-					<Token {token} rawString noPad {color} />
+		<div class="col-span-8">
+			<BorderSection title="Tokens">
+				<div class=" flex flex-row flex-wrap gap-0 gap-y-4">
+					{#each tokens as token, index}
+						{@const color = token.isByte()
+							? 'gray'
+							: (['pastelBlue', 'pastelPink', 'pastelYellow', 'pastelGreen'] as const)[index % 4]}
+						<div>
+							<Token {token} rawString noPad {color} />
+						</div>
+					{/each}
 				</div>
-			{/each}
+			</BorderSection>
 		</div>
 	</div>
 </div>
