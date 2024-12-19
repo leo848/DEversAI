7b2cb30e7b4e60504a5b6457e0b425d0250af051
L. Blume <blume.l@posteo.de>
2024-12-19 12:21:38 +0100

    New grid layout for tokenize page

--- frontend/src/routes/tokenize/+page.svelte
+++ frontend/src/routes/tokenize/+page.svelte
@@ -10,20 +10,25 @@
 <div class="m-4 flex flex-col gap-8 xl:mx-16">
 	<div class="text-4xl font-bold">Tokenisierung</div>
 
-	<textarea
-		class="w-full rounded-xl border-2 border-gray-200 p-4 text-2xl transition-all focus:border-gray-500"
-		bind:value={string}
-	>
-	</textarea>
+	<div class="grid grid-cols-12 gap-8">
+		<div class="col-span-8">
+			<textarea
+				class="w-full rounded-xl border-2 border-gray-200 p-4 text-2xl transition-all focus:border-gray-500"
+				bind:value={string}
+			>
+			</textarea>
+		</div>
+		<div class="col-span-4 row-span-2"></div>
 
-	<div class="flex flex-row flex-wrap gap-0 gap-y-4">
-		{#each tokens as token, index}
-			{@const color = token.isByte()
-				? 'gray'
-				: (['pastelBlue', 'pastelPink', 'pastelYellow', 'pastelGreen'] as const)[index % 4]}
-			<div>
-				<Token {token} rawString noPad {color} />
-			</div>
-		{/each}
+		<div class="col-span-8 flex flex-row flex-wrap gap-0 gap-y-4">
+			{#each tokens as token, index}
+				{@const color = token.isByte()
+					? 'gray'
+					: (['pastelBlue', 'pastelPink', 'pastelYellow', 'pastelGreen'] as const)[index % 4]}
+				<div>
+					<Token {token} rawString noPad {color} />
+				</div>
+			{/each}
+		</div>
 	</div>
 </div>
