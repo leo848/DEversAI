fd175b6de5a44df3036111459f1e69dd8afc2b00
L. Blume <blume.l@posteo.de>
2025-01-04 16:39:56 +0100

    Snippetize token list search

--- frontend/src/lib/components/TokenList.svelte
+++ frontend/src/lib/components/TokenList.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import type { Vocabulary } from '$lib/tokenizing/vocabulary';
 	import { Token } from '$lib/tokenizing/token';
 	import TokenComponent from '$lib/components/Token.svelte';
+	import {flip} from 'svelte/animate';
 
 	const {
 		vocabulary,
+		pageNumber,
 		tokens = null,
+		href = (_) => null,
 		searchable = true,
 		sortable = true,
 		tokensPerPage = 1000
 	}: {
 		vocabulary: Vocabulary;
+		pageNumber: number;
 		tokens?: null | Token[];
 		searchable?: boolean;
 		sortable?: boolean;
+		href?: (page: number) => (string | null);
 		tokensPerPage?: number;
 	} = $props();
 
@@ -35,14 +39,13 @@
 		resultingTokens.slice((pageNumber - 1) * tokensPerPage, pageNumber * tokensPerPage)
 	);
 
-	const pageNumber = $derived(Number($page.url.searchParams.get('p') ?? 2));
 	let pageAmount = $derived(Math.ceil(resultingTokens.length / tokensPerPage));
 
 	let pagination = $derived(
 		(() => {
-			const start = [1, 2, 3];
-			const context = [pageNumber - 1, pageNumber, pageNumber + 1];
-			const end = [pageAmount, pageAmount - 1, pageAmount - 2];
+			const start = [1, 2];
+			const context = [pageNumber - 2, pageNumber - 1, pageNumber, pageNumber + 1, pageNumber + 2];
+			const end = [pageAmount, pageAmount - 1];
 			const array = [...start, ...context, ...end].filter((elem) => elem > 0 && elem <= pageAmount);
 			array.sort((a, b) => a - b);
 			const unique = [...new Set(array)].map((index) => ({
@@ -65,15 +68,32 @@
 			/>
 		</div>
 	{/if}
+	{#snippet paginationRow()}
 	<div class="flex flex-row justify-center gap-2">
-		{#each pagination as pageLink}
+		{#each pagination as pageLink, index (pageLink.number)}
+			{#if index != 0 && pagination[index].number - pagination[index - 1].number != 1}
+				{#if pagination[index].number - pagination[index - 1].number == 2}
+				<a
+					class="w-10 h-10 flex flex-row items-center justify-center rounded-lg border-2 border-gray-200 p-2 text-center text-xl transition-all hover:bg-gray-200"
+					href={href(pageLink.number - 1)}
+					class:bg-gray-200={pageLink.number - 1 == pageNumber}>{pageLink.number - 1}</a
+				>
+				{:else}
+				<div
+					class="w-10 h-10 flex flex-row items-center justify-center rounded-lg border-2 border-gray-200 p-2 text-center text-xl transition-all"
+					>...</div
+				>
+				{/if}
+			{/if}
 			<a
-				class="w-10 rounded-lg border-2 border-gray-200 p-2 text-center text-xl transition-all hover:bg-gray-200"
-				href={`/tokens/list?p=${pageLink.number}`}
+				class="w-10 h-10 flex flex-row items-center justify-center rounded-lg border-2 border-gray-200 p-2 text-center text-xl transition-all hover:bg-gray-200"
+				href={href(pageLink.number)}
 				class:bg-gray-200={pageLink.number == pageNumber}>{pageLink.number}</a
 			>
 		{/each}
 	</div>
+	{/snippet}
+	{@render paginationRow()}
 	<div>
 		<div class="flex flex-row flex-wrap gap-2">
 			{#each shownTokens as token}
@@ -81,13 +101,5 @@
 			{/each}
 		</div>
 	</div>
-	<div class="flex flex-row justify-center gap-2">
-		{#each pagination as pageLink}
-			<a
-				class="w-10 rounded-lg border-2 border-gray-200 p-2 text-center text-xl transition-all hover:bg-gray-200"
-				href={`/tokens/list?p=${pageLink.number}`}
-				class:bg-gray-200={pageLink.number == pageNumber}>{pageLink.number}</a
-			>
-		{/each}
-	</div>
+	{@render paginationRow()}
 </div>
--- frontend/src/routes/tokens/list/+page.svelte
+++ frontend/src/routes/tokens/list/+page.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import TokenList from '$lib/components/TokenList.svelte';
 	import vocabulary from '$lib/tokenizing/german50000';
+	import { page } from '$app/state';
+
+	let pageNumber = $derived(Number(page.url.searchParams.get('p') ?? 1));
 </script>
 
 <div class="m-4 flex flex-col gap-8 xl:mx-16">
 	<div class="text-4xl font-bold">Token-Liste</div>
-	<TokenList {vocabulary} />
+	<TokenList {vocabulary} {pageNumber} href={n => `/tokens/list?p=${n}`} />
 	<div class="h-20"></div>
 </div>
