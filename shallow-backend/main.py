4a748e86431e30d3068aa789dc534994a8e7b2bf
L. Blume <blume.l@posteo.de>
2025-01-12 13:58:08 +0100

    Add root status ok route

--- shallow-backend/main.py
+++ shallow-backend/main.py
@@ -23,6 +23,10 @@ def load_token_infos() -> dict[int, list[Tuple[str, str]]]:
     with open("assets/example_strings.json") as f:
         return json.load(f)
 
+@app.get("/")
+def root_route():
+    return { "status": "OK" }
+
 @app.get("/v0/token/{token_id}/info")
 def get_token_examples(token_id: int, token_examples: Annotated[dict, Depends(load_token_infos)]):
     examples = token_examples.get(str(token_id))
