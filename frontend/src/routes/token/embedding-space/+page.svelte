77602a551813c0cacabd59c404a753561e3891aa
L. Blume <blume.l@posteo.de>
2025-01-28 11:37:58 +0100

    Use syntactical categories to cluster tokens

--- frontend/src/routes/token/embedding-space/+page.svelte
+++ frontend/src/routes/token/embedding-space/+page.svelte
@@ -2,7 +2,7 @@
 	import { Client } from '$lib/backend/client';
 	import vocabulary from '$lib/tokenizing/german50000';
 	import type { Tuple } from '$lib/util/array';
-	import { Gradient } from '$lib/util/color';
+	import { Color, Gradient } from '$lib/util/color';
 	import ScatterPlot3D from './ScatterPlot3D.svelte';
 
 	const client = new Client();
@@ -11,12 +11,26 @@
 	const sampleColor = (t: number) => Gradient.Viridis.sample(t).rgb();
 
 	const toData = (data: Tuple<3, number>[]) => {
-		return data.map((pos, i) => ({
-			id: i,
-			position: pos,
-			// color: sampleColor(1 - Math.min(10, vocabulary.tokens[i].value.length - 1) / 10)
-			color: sampleColor(i / 50256)
-		}));
+        return data.map((pos, i) => {
+          const bytes = vocabulary.tokens[i].value;
+          const commonSuffixes = [32, 114, 110, 116, 108, 115, 10, 104, 101, 103];
+          const interestingSuffixes = [32, 10, 45, 46, 47, 40, 44, 58, 95, 34,];
+
+          const commonPrefixes = [101, 97, 115, 83, 65, 103, 105, 98, 66, 100,]
+          const interestingPrefixes = [10, 46, 45, 40, 58, 44, 32, 41, 63, 33];
+
+          const categoryIndex = interestingSuffixes.findIndex(byte => bytes[bytes.length - 1] == byte);
+          // const categoryIndex = interestingPrefixes.findIndex(byte => bytes[0] == byte);
+
+          const color = categoryIndex == -1 ? [238, 238, 238] : Color.Category10[categoryIndex].rgb()
+          return {
+              id: i,
+              position: pos,
+              // color: sampleColor(1 - Math.min(10, vocabulary.tokens[i].value.length - 1) / 10)
+              // color: sampleColor(i / 50256)
+              color,
+          };
+        });
 	};
 </script>
 
@@ -24,7 +38,6 @@
 	{#await embeddingData}
 		Loading data...
 	{:then object}
-		{@debug object}
 		<ScatterPlot3D points={toData(object.embeddings3D)} initialZoom={5} />
 	{:catch error}
 		Fehler: {error}
