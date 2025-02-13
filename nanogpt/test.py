f284846196ff35d3d94ccd9da1eab0297406fb6e
L. Blume <blume.l@posteo.de>
2025-02-13 17:06:48 +0100

    Change type to int64

--- nanogpt/test.py
+++ nanogpt/test.py
@@ -60,8 +60,8 @@ with torch.no_grad(), ctx:
         num_batches = 0
 
         for start_i in tqdm(range(0, len(data) - block_size - batch_size, batch_size - 1)):
-            x = torch.stack([torch.from_numpy(data[i:i + block_size].astype(np.dtype("<u2"))) for i in range(start_i, start_i + block_size)])
-            y = torch.stack([torch.from_numpy(data[i + 1: i + 1 + block_size].astype(np.dtype("<u2"))) for i in range(start_i + 1, start_i + 1 + block_size)])
+            x = torch.stack([torch.from_numpy(data[i:i + block_size].astype(np.int64)) for i in range(start_i, start_i + block_size)])
+            y = torch.stack([torch.from_numpy(data[i + 1: i + 1 + block_size].astype(np.int64)) for i in range(start_i + 1, start_i + 1 + block_size)])
             x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
             _, loss = model(x, y)
             total_loss += loss
