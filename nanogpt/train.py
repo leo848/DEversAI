2b84e58b3e3e26cfdae34be6faec74ed454875d8
L. Blume <blume.l@posteo.de>
2025-02-14 11:32:52 +0100

    Fix train error

--- nanogpt/train.py
+++ nanogpt/train.py
@@ -43,8 +43,8 @@ checkpoint_interval = 2500
 eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
-init_from_resume_checkpoint = 95000
+init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+init_from_resume_checkpoint = None
 # data
 dataset = 'custom'
 gradient_accumulation_steps = 6 * 8 # used to simulate larger batch sizes
