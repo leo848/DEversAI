2d0c9e798c9809b0389f680d4f047d2d087edf82
L. Blume <blume.l@posteo.de>
2025-02-15 23:09:01 +0100

    Update test for another round of testing

--- nanogpt/test.py
+++ nanogpt/test.py
@@ -15,11 +15,11 @@ seed = random.randint(0, int(1e10))
 print(f"Using seed {seed}")
 
 causality = "causal"
-ckpt_value = 100000
+ckpt_value = 25000
 file_prefix = "oscar-2301"
 device = 'cuda:2' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1', etc.
 
-files = [f"{file_prefix}-shard-00012.bin"]
+files = [f"{file_prefix}-shard-00002.bin", f"{file_prefix}-shard-00012.bin", f"{file_prefix}-shard-00022.bin"]
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16' # 'float32' or 'bfloat16' or 'float16'
 compile = True # use PyTorch 2.0 to compile the model to be faster
 batch_size = 64
