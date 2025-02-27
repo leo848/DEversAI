9a88a8749e5c14aafffb98eff6cd8a4ba03871fa
L. Blume <blume.l@posteo.de>
2025-02-27 08:20:17 +0100

    Improve logit request interface

---
+++ frontend/src/lib/components/EmergentSpinner.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import {Color} from "$lib/util/color";
+
+	let {
+		size = "50px",
+		color = Color.luma(0.3)
+	}: {
+		size?: string,
+		color?: Color,
+	} = $props()
+</script>
+
+<style>
+  .spinner-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .spinner {
+    border: 4px solid rgba(255, 255, 255, 0.3);
+    border-top: 4px solid var(--spinner-color);
+    border-radius: 50%;
+    width: var(--spinner-size);
+    height: var(--spinner-size);
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    from { transform: rotate(0deg); }
+    to { transform: rotate(360deg); }
+  }
+</style>
+
+<div class="spinner-container">
+  <div class="spinner" style="--spinner-size: {size}; --spinner-color: {color.toString()};"></div>
+</div>
--- frontend/src/routes/chat/+page.svelte
+++ frontend/src/routes/chat/+page.svelte
@@ -1,40 +1,51 @@
 <script lang="ts">
 	import { Client } from '$lib/backend/client';
-	import { LogitsResponse } from '$lib/backend/types';
+	import EmergentSpinner from '$lib/components/EmergentSpinner.svelte';
 	import TopLogits from '$lib/components/TopLogits.svelte';
 	import vocabulary from '$lib/tokenizing/german50000';
+	import {slide} from 'svelte/transition';
 
 	const client = new Client();
 
 	const initialValue = 'In der Pfanne ausbacken. ';
 
 	let inputString = $state(initialValue);
-	let processString = $state(initialValue);
 
-	let tokens = $derived(vocabulary.tokenize(processString));
-
-	let logitsInference = $derived({
-		anticausal1: () => client.modelLogits('anticausal1', tokens.toReversed()),
-		causal1: () => client.modelLogits('causal1', tokens)
+	let processString = $state({
+		anticausal1: initialValue,
+		causal1: initialValue,
 	});
 
-	let changeProcessTimeout: null | number = null;
-	$effect(() => {
-		inputString;
-		if (changeProcessTimeout != null) clearTimeout(changeProcessTimeout);
-		changeProcessTimeout = setTimeout(() => {
-			processString = inputString;
-		}, 1000);
+	let tokens = $state({
+		anticausal1: vocabulary.tokenize(initialValue),
+		causal1: vocabulary.tokenize(initialValue),
 	});
+
+	let logitsInference = $state({
+		anticausal1: () => client.modelLogits('anticausal1', tokens.anticausal1.toReversed()),
+		causal1: () => client.modelLogits('causal1', tokens.causal1)
+	});
+
+	function refreshModel(modelName: "anticausal1" | "causal1") {
+		processString[modelName] = inputString;
+		tokens[modelName] = vocabulary.tokenize(processString[modelName]);
+		let modelInput = modelName.includes("anti") ? tokens[modelName].toReversed() : tokens[modelName];
+		logitsInference[modelName] = () => client.modelLogits(modelName, modelInput);
+	}
 </script>
 
 <div class="m-4 flex flex-col gap-8 xl:mx-16">
 	<div class="text-4xl font-bold">Inferenz</div>
 
 	<div class="grid grid-cols-3 gap-4">
-		<div class="min-w-200">
+		<div class="min-w-200 border-2 border border-gray-200 rounded-xl p-2">
+			{#if inputString != processString.anticausal1}
+				<div class="w-full p-2 text-xl" transition:slide={{axis: "y"}}>
+					<button class="bg-fire-400 p-2 rounded" onclick={() => refreshModel("anticausal1")}>Aktualisieren</button>
+				</div>
+			{/if}
 			{#await logitsInference.anticausal1()}
-				Lade Logits...
+				<EmergentSpinner />
 			{:then logitsResponse}
 				<TopLogits
 					{logitsResponse}
@@ -48,14 +59,19 @@
 		</div>
 		<div class="w-full">
 			<textarea
-				class="w-full resize-none overflow-scroll rounded border-2 border-gray-200 text-xl focus:border-gray-400"
+				class="w-full resize-none overflow-scroll rounded-xl border-2 border-gray-200 text-xl focus:border-gray-400"
 				rows={10}
 				bind:value={inputString}
 			></textarea>
 		</div>
-		<div class="min-w-200">
+		<div class="min-w-200 border-2 border border-gray-200 rounded-xl p-2">
+			{#if inputString != processString.causal1}
+				<div class="w-full p-2 text-xl" transition:slide={{axis: "y"}}>
+					<button class="bg-fire-400 p-2 rounded" onclick={() => refreshModel("causal1")}>Aktualisieren</button>
+				</div>
+			{/if}
 			{#await logitsInference.causal1()}
-				Lade Logits...
+				<EmergentSpinner />
 			{:then logitsResponse}
 				<TopLogits
 					{logitsResponse}
