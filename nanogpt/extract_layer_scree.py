12f96f33f6b94eb3662950826e1550cdc1d22185
L. Blume <blume.l@posteo.de>
2025-03-06 16:33:25 +0100

    Add scree plot for poster

---
+++ nanogpt/extract_layer_scree.py
@@ -0,0 +1,39 @@
+from sklearn.decomposition import PCA
+from tqdm import tqdm
+import json
+import bracex
+import numpy as np
+
+last_file = None
+def pca_variance(file):
+    global last_file
+    if file is not None:
+        last_file = file
+        embedding = np.load(file)
+    elif last_file is not None:
+        embedding = np.load(last_file).reshape((50256 * 768,))
+        np.random.shuffle(embedding)
+        embedding = embedding.reshape((50256, 768))
+    else:
+        exit(1)
+    print(embedding.shape)
+    pca = PCA()
+    pca.fit_transform(embedding)
+    return np.cumsum(pca.explained_variance_ratio_) * (-1) + 1
+
+def main():
+    # reference_variance = pca_variance("output/causal1.pt-embedding-768d.npy")
+    result = {}
+    for file in tqdm([*bracex.expand("output/{anti,}causal1{,-laws1,-plenar1}.pt-embedding-768d.npy"), None]):
+        variance = pca_variance(file)
+        if file is not None:
+            key = file.split(".pt")[0].split("/")[-1]
+        else:
+            key = "random"
+        result[key] = variance.tolist()
+
+    with open("output/wpe-scree.json", "w") as f:
+        json.dump(result, f)
+
+if __name__ == "__main__":
+    main()
