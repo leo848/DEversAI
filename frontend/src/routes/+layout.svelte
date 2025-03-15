d454cfe9a5a0d27e5575bc1eacdcfa3086ee25fe
L. Blume <blume.l@posteo.de>
2025-03-15 22:55:38 +0100

    Add Jugend forscht logo to footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -15,14 +15,37 @@
 		</main>
 		<footer>
 			<hr class="border-1 mb-3 w-full border border-gray-200" />
-			<div class="flex flex-row">
-				<div>
+			<div class="grid grid-cols-6">
+				<div class="col-span-6 flex flex-col gap-2 xl:col-span-2">
 					<DeversaiLogo />
-				</div>
-				<div>
 					<Deployment />
+				</div>
+				<div class="col-span-6 md:col-span-3 xl:col-span-2">
+					<a href="https://jugend-forscht.de" target="_blank">
+						<img
+							class="bg-black-transparent mb-4 rounded-md p-2"
+							src="/1920px-Jugend_forscht.svg"
+							alt="Jugend forscht logo"
+						/>
+					</a>
+					<div class="">Ein <b>jugend forscht</b>-Projekt von Leo Blume</div>
+					<div class="opacity-50">
+						Copyright &copy; 2024, 2025 Leo Blume. Der Quelltext des Projekts (Frontend und Backend)
+						steht unter der GNU GPL v3-Lizenz und ist damit freie Software; er kann unter den
+						Bedingungen dieser Lizenz geteilt und/oder modifiziert werden.
+					</div>
 				</div>
 			</div>
 		</footer>
 	</div>
 </div>
+
+<style>
+	.bg-black-transparent {
+		background-color: rgba(0, 0, 0, 10%);
+		transition: all 0.1s;
+	}
+	.bg-black-transparent:hover {
+		background-color: rgba(0, 0, 0, 30%);
+	}
+</style>
