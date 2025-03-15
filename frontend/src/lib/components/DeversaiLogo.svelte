2ee644aa09038bf03846d7947e592f0418b90868
L. Blume <blume.l@posteo.de>
2025-03-15 18:52:30 +0100

    Add DEversAI logo

---
+++ frontend/src/lib/components/DeversaiLogo.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Gradient } from '$lib/util/color';
+</script>
+
+<a
+	class="deversai-logo display-block group m-4 inline-block w-[444px] rounded-2xl border border-4 border-gray-200 p-8 text-left text-white transition-all hover:scale-105"
+	href="/"
+	style:background={Gradient.Viridis.reverse().css('-45deg')}
+>
+	<div class="group-hover:rotate-x-50 group-hover:rotate-x-15 text-4xl font-bold transition-all">
+		DEversAI
+	</div>
+	<div class="text-2xl">
+		Training und Visualisierung<br />
+		deutsch lokalisierter<br />
+		direktionalkomplementärer LLMs
+	</div>
+</a>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
 	import '../app.css';
 	import NavBar from '$lib/components/NavBar.svelte';
+	import DeversaiLogo from '$lib/components/DeversaiLogo.svelte';
 
 	let { children } = $props();
 </script>
 
 <div class="flex h-full flex-row">
 	<NavBar />
-	<main class="mb-[10rem] grow">
-		{@render children()}
-	</main>
+	<div class="grow">
+		<main class="mb-[10rem] min-h-[100vh]">
+			{@render children()}
+		</main>
+		<footer>
+			<div>
+				<hr class="border-1 mb-3 w-full border border-gray-200" />
+				<DeversaiLogo />
+			</div>
+		</footer>
+	</div>
 </div>
