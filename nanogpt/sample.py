810f930ff2d1058c69347419f6d7eacf7f3bdd61
L. Blume <blume.l@posteo.de>
2025-03-21 09:17:09 +0100

    Prepare for restart, sample example

--- nanogpt/sample.py
+++ nanogpt/sample.py
@@ -12,8 +12,8 @@ from vocabulary import Vocabulary
 from torch.nn import functional as F
 
 # -----------------------------------------------------------------------------
-model_name = "causal-fw2-ckpt27500.pt"
-vocab_file = "fineweb2.vocab"
+model_name = "causal1.pt"
+vocab_file = "german-complete.vocab"
 
 compile = False # use PyTorch 2.0 to compile the model to be faster
 causality = "anticausal" if "anticausal" in model_name else "causal" # 'causal' or 'anticausal'
@@ -29,7 +29,7 @@ show_samples_json = False
 # config
 
 num_samples = 64 # number of samples to draw
-max_new_tokens = int(1e10) # number of tokens generated in each sample
+max_new_tokens = int(100) # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 
--- nanogpt/train.py
+++ nanogpt/train.py
@@ -40,8 +40,8 @@ checkpoint_interval = 2500
 eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
-init_from_resume_checkpoint = None
+init_from = 'resume' # 'scratch' or 'resume' or 'gpt2*'
+init_from_resume_checkpoint = 270000
 # data
 dataset = 'custom'
 gradient_accumulation_steps = 6 * 8 # used to simulate larger batch sizes
@@ -55,7 +55,7 @@ dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 # adamw optimizer
 learning_rate = 6e-4 # max learning rate
-max_iters = 400_000 # total number of training iterations
+max_iters = 300_000 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
 beta2 = 0.95
