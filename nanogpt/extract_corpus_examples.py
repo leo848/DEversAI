9039c38aff63c6a55ab6a7e2804184e396d36c4b
L. Blume <blume.l@posteo.de>
2025-05-11 15:02:17 +0200

    Use /data

--- nanogpt/extract_corpus_examples.py
+++ nanogpt/extract_corpus_examples.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-FILE = "/input/fw2-tokenized/train/fw2-shard-00069.bin"
+FILE = "/data/fw2-tokenized/train/fw2-shard-00069.bin"
 
 data = np.memmap(FILE, dtype=np.dtype(">u2"))
 
