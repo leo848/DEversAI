f60bd971d2afa5a9763965346766df7ae0b8ba6e
L. Blume <blume.l@posteo.de>
2025-05-12 10:51:12 +0200

    Max two sequential newlines

--- nanogpt/extract_gutenberg.py
+++ nanogpt/extract_gutenberg.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 
 BR_KEY = "␤nl␤"
 EXPR = re.compile(r"\s+")
+EXPR_NL = re.compile(r"\n{2,}")
 def strip(text: str):
     return re.sub(EXPR, " ", text).replace(BR_KEY, "\n").strip()
 
@@ -100,6 +101,7 @@ def main():
                     body_paragraphs.append(text)
 
         content = "\n".join(body_paragraphs)
+        content = EXPR_NL.sub(content, "\n\n")
 
         if len(content) < 100:
             continue
