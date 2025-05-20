22435484e543a272d8f7cf43ccd1191ff20b082d
L. Blume <blume.l@posteo.de>
2025-05-20 18:33:06 +0200

    Host new models

--- deep-backend/main.py
+++ deep-backend/main.py
@@ -20,6 +20,10 @@ MODEL_LOCATIONS = [
     ModelLocation("causal1", 300_000, 9),
     ModelLocation("anticausal-fw2", 300_000, 9),
     ModelLocation("causal-fw2", 300_000, 9),
+    ModelLocation("anticausal-fw2-laws1", 301_000, 9),
+    ModelLocation("causal-fw2-laws1", 301_000, 9),
+    ModelLocation("anticausal-fw2-wikipedia1", 400_000, 9),
+    ModelLocation("causal-fw2-wikipedia1", 400_000, 9),
 ]
 
 # Load models and assign a unique CUDA Stream to each
