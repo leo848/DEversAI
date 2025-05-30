26d4d9ccad184b90fbfd7b0afb42386c99c6b2fb
L. Blume <blume.l@posteo.de>
2025-05-30 14:14:39 +0200

    Smaller icons in NavBar for mobile devices

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -3,8 +3,8 @@
 	import Icon from '@iconify/svelte';
 </script>
 
-<nav class="group/navbar min-h-[100vh] w-20 transition-all duration-300 hover:w-64">
-	<div class="fancy-gradient-bg flex h-full flex-col gap-4 overflow-hidden p-2">
+<nav class="group/navbar min-h-[100vh] md:w-20 w-12 transition-all duration-300 hover:w-64">
+	<div class="fancy-gradient-bg flex h-full w-full flex-col gap-4 overflow-hidden p-2">
 		{#each pages as page, index}
 			{@const extern = !page.link.startsWith('/') || page.link.endsWith('pdf')}
 			<div>
@@ -14,12 +14,12 @@
 					class="group/link flex flex-row gap-4"
 				>
 					<div
-						class="relative inline-block rounded-xl bg-white bg-opacity-50 p-2 transition-all group-hover/link:bg-opacity-100"
+						class="relative inline-block rounded-xl bg-white bg-opacity-50 xl:p-2 p-1 transition-all group-hover/link:bg-opacity-100"
 					>
-						<Icon {...page.icon} width="3rem" />
+						<Icon {...page.icon} class="xl:text-4xl text-2xl" />
 						{#if extern}
 							<div
-								class="align-center absolute -end-2 -top-2 inline-flex h-6 w-6 items-center justify-center rounded-xl bg-white bg-opacity-40 group-hover/link:bg-opacity-90"
+								class="align-center absolute -end-2 -top-2 inline-flex md:h-6 md:w-6 h-3 w-3 items-center justify-center rounded-xl bg-white bg-opacity-40 group-hover/link:bg-opacity-90"
 							>
 								<Icon icon="iconamoon:arrow-top-right-1-bold" width="1.5rem" />
 							</div>
